fcbb1a54cf0554d43ca6068d47861a64e9c1e7fc
Josef Berthoud <josef.berthoud@gmail.com>
2019-08-03 14:47:13 -0700

    Add restaurant endpoint for reservations service

--- server/index.js
+++ server/index.js
@@ -10,29 +10,34 @@ const proxy = httpProxy.createProxyServer();
 app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, '/../client')));
 
-const gallery = 'http://localhost:3000';
+// const gallery = 'http://localhost:3000';
 const reservation = 'http://localhost:3001';
-const popular = 'http://localhost:3002';
-const header = 'http://localhost:3003';
+// const popular = 'http://localhost:3002';
+// const header = 'http://localhost:3003';
 
-app.all('/gallery/:id', function(req, res) {
-    console.log('redirecting to gallery');
-    proxy.web(req, res, { target: gallery });
-});
+// app.all('/:id/gallery', function(req, res) {
+//     console.log('redirecting to gallery');
+//     proxy.web(req, res, { target: gallery });
+// });
 
-app.all('/reservation/:id', function(req, res) {
+app.all('/:id/reservation', function(req, res) {
     console.log('redirecting to reservation');
     proxy.web(req, res, { target: reservation });
 });
 
-app.all('/popular/:id', function(req, res) {
-    console.log('redirecting to popular');
-    proxy.web(req, res, { target: popular });
+app.all('/:id/restaurant', function(req, res) {
+    console.log('redirecting to reservation');
+    proxy.web(req, res, { target: reservation });
 });
 
-app.all('/header/:id', function(req, res) {
-    console.log('redirecting to header');
-    proxy.web(req, res, { target: header });
-});
+// app.all('/:id/popular', function(req, res) {
+//     console.log('redirecting to popular');
+//     proxy.web(req, res, { target: popular });
+// });
+
+// app.all('/:id/header', function(req, res) {
+//     console.log('redirecting to header');
+//     proxy.web(req, res, { target: header });
+// });
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
